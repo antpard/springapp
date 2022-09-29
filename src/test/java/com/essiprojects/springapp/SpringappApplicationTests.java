2f3bf570355163df7df2b9366bede071be22fe7c
Antonio Pardo <apardo@essiprojects.com>
2022-09-29 18:03:20 +0200

    first commit

---
+++ src/test/java/com/essiprojects/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essiprojects.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
